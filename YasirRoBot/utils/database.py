3f6d71de0f143ecc512963916ce40bf47b5d9787
yasirarism <55983182+yasirarism@users.noreply.github.com>
2022-11-27 11:01:25 +0700

    FIx DB

--- YasirRoBot/utils/database.py
+++ YasirRoBot/utils/database.py
@@ -8,19 +8,20 @@ class Database:
         self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
         self.db = self._client[database_name]
         self.col = self.db.users
+        self.ban = self.db.ban_user
 
     def new_user(self, id):
         return dict(id=id, join_date=datetime.date.today().isoformat())
 
     async def add_ban_user(self, id):
-        await self.col.insert_one({"id": int(id)})
+        await self.ban.insert_one({"id": int(id)})
 
     async def is_banned(self, id):
-        user = await self.col.find_one({"id": int(id)})
+        user = await self.ban.find_one({"id": int(id)})
         return bool(user)
 
     async def remove_ban(self, user_id):
-        await self.col.delete_many({"id": int(user_id)})
+        await self.ban.delete_many({"id": int(user_id)})
 
     async def add_user(self, id):
         user = self.new_user(id)
