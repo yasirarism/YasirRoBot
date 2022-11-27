50dc7b30d8a46a24dec64c916810c2b3d6d2b374
yasirarism <55983182+yasirarism@users.noreply.github.com>
2022-11-27 10:41:11 +0700

    list

--- YasirRoBot/bot/plugins/ban_user.py
+++ YasirRoBot/bot/plugins/ban_user.py
@@ -6,7 +6,7 @@ from YasirRoBot.utils.database import Database
 db = Database(Var.DATABASE_URL, Var.name)
 
 
-@StreamBot.on_message(filters.command("ban") & filters.private & filters.user(Var.OWNER_ID))
+@StreamBot.on_message(filters.command("ban") & filters.private & filters.user(list(Var.OWNER_ID)))
 async def ban_handler(bot, m):
     if len(m.command) == 1:
         await m.reply_text(text="🚫 Gunakan: /ban [id user] untuk membanned pengguna.")
@@ -19,7 +19,7 @@ async def ban_handler(bot, m):
         await m.reply(f"🚫 User {user} sudah dibanned sebelumnya.")
 
 
-@StreamBot.on_message(filters.command("unban") & filters.private & filters.user(Var.OWNER_ID))
+@StreamBot.on_message(filters.command("unban") & filters.private & filters.user(list(Var.OWNER_ID)))
 async def rm_ban_handler(bot, m):
     if len(m.command) == 1:
         await m.reply_text(text="🚫 Gunakan: /unban [id user] untuk unban pengguna.")
